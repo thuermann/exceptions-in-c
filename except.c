a9c00b84d57bc06c065f59916bcb53862cead2d4
Urs Thuermann <urs@isnogud.escape.de>
2016-02-20 07:31:11 +0100

    Add usage info to README and a comment

--- except.c
+++ except.c
@@ -1,10 +1,18 @@
 /*
- * $Id: except.c,v 1.1 2016/02/20 06:28:48 urs Exp $
+ * $Id: except.c,v 1.2 2016/02/20 06:31:11 urs Exp $
  */
 
 #include <stdlib.h>
 
 #include "except.h"
+
+/* We store a linked list of setjmp buffers representing the nested
+ * TRY blocks.  Each TRY inserts a new jump buffer at the list's head
+ * that THROW() uses to jump to the CATCH block of the first TRY on
+ * the list.  Following the TRY block, either after executing the
+ * block or after catching an exception, the CATCH macro removes the
+ * head from the jump buffer list.
+ */
 
 struct jblist *jblist;
 
